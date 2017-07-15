fa01c5b3fc03e5a9554ad70975e0576b33b658ba
Maksym Ciechomski <maksym.ciechomski@gmail.com>
2017-07-15 14:30:21 +0200

    Preparation of the environment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+module.exports = {
+  entry: "./js/app.jsx",
+  output: {
+    path: path.resolve("js"),
+    filename: "app.js"
+  },
+  devServer: {
+    inline: true,
+    contentBase: './',
+    port: 3001
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/, exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: [ 'es2015',
+        'stage-2',
+        'react'] }
+      },
+    {
+      test: /\.html$/,
+      loaders: ['raw-loader']
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader','sass-loader']
+    }
+    ]
+  }
+}
